b200f75aa720c530b67e3d4ea74e82e8b8eed996
Valeria Vanhelieva <vangeljeva.valeria@gmail.com>
2020-07-21 02:18:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
  var price = document.getElementsByClassName("price");
+  var toggle = document.getElementById("toggle-button");
+
   function changeData() {
     for(var i = 1; i<price.length; i+=2) {
       price[i-1].classList.toggle("hidden");
@@ -6,13 +8,13 @@
     }
   }
 
-  var toggle = document.getElementById("toggle-button");
+ 
 
    document.onkeydown = function(e){
-      if(e.keyCode === 39){
+      if(e.keyCode === 39 && !toggle.checked){
         toggle.checked = true; 
         changeData();
-      } else if(e.keyCode === 37){
+      } else if(e.keyCode === 37 && toggle.checked){
         toggle.checked = false; 
         changeData();
       } 
